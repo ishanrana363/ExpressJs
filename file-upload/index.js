f91c1d9bf4cecf2d8f1b84178c26333e8f4adcde
ishanrana363 <ishanrana363@gmail.com>
2025-03-08 19:29:57 +0600

    added

--- file-upload/index.js
+++ file-upload/index.js
@@ -2,10 +2,15 @@ const express = require("express");
 const multer = require('multer')
 const app = new express();
 const port = 3000;
+const fs = require("fs");
 
 const path = require("path");
 
 app.use(express.static("public"));
+app.use("uploads",express.static("/uploads"));
+app.get("/",(req,res)=>{
+    res.sendFile( process.cwd() + "/public/index.html" )
+})
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, 'uploads/')
@@ -15,7 +20,9 @@ const storage = multer.diskStorage({
     }
 })
 
-const upload = multer({ storage: storage });
+const upload = multer({ storage: storage, limits:{
+    fileSize : 1024*1024*1
+} });
 
 app.post("/upload",upload.single("image"),(req,res)=>{
     if(!req.file){
@@ -29,6 +36,21 @@ app.post("/upload",upload.single("image"),(req,res)=>{
         file : req.file.filename
     })
 });
+
+
+app.delete("/file-delete/:fileName",(req,res)=>{
+    const filePath = path.join(process.cwd(),"/uploads", req.params.fileName );
+    fs.unlink(filePath,(err)=>{
+        if(err) return res.status(500).json({
+            status : "fail",
+            msg : "File delete fail"
+        })
+        res.status(200).json({
+            status:"success",
+            msg : "File delete successfully"
+        })
+    })
+})
 
 
 
